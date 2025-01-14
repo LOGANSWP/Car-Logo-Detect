7e76915ac0a74912a7225bf70d37baf3efc883d3
LoganSu <logansu1025@outlook.com>
2025-01-14 14:56:09 +0800

    【Feature】 Can pick a photo from Photos Library

--- Car-Logo-Detect/Functions/PhotosViewController.swift
+++ Car-Logo-Detect/Functions/PhotosViewController.swift
@@ -7,7 +7,35 @@
 
 import Foundation
 import UIKit
+import SnapKit
+import ZLPhotoBrowser
 
 class PhotosViewController: UIViewController {
+    private lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        
+        let ps = ZLPhotoPreviewSheet()
+        ps.selectImageBlock = { [weak self] results, isOriginal in
+            guard let self else { return }
+            imageView.image = results[0].image
+        }
+        ps.showPhotoLibrary(sender: self)
+    }
+    
+    private func setupViews() {
+        view.addSubview(imageView)
+        
+        imageView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.height.equalTo(200)
+        }
+    }
 }
